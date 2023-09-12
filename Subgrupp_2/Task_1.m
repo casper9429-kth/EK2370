0e26057bae815c039b71f54b54e6bca8bdddce7f
Rayan Cali <raycabcal@gmail.com>
2023-09-12 20:12:39 +0200

    DONE

--- Subgrupp_2/Task_1.m
+++ Subgrupp_2/Task_1.m
@@ -18,7 +18,7 @@ Sample_per_sweep=Tp/T;
 M = N/Sample_per_sweep;
 
 %Creating a 2D array
-First_array = zeros(round(M)-1,Sample_per_sweep+4*Sample_per_sweep);
+First_array = zeros(round(M)-1,Sample_per_sweep+5*Sample_per_sweep);
 
 %Variable to keep track of each sample
 k = 1;
@@ -26,21 +26,22 @@ velocities = linspace(0,sampleRate/2, 2*Sample_per_sweep);
 velocities = velocities * (3*10^8)/(2 * 2.43 * 10^9);
 
 timearray = linspace(0,Tp*(round(M)-1),round(M)-1);
-round(M)
-audioData_inv = reshape(audioData_inv(1:1900710), [Sample_per_sweep, round(M)-1]);
+
+
+audioData_inv = reshape(audioData_inv(1:Sample_per_sweep*(round(M)-1)), [Sample_per_sweep, round(M)-1]);
 First_array(:, 1:Sample_per_sweep) = audioData_inv';
 
 %clutter rej
-%First_array(:,1:Sample_per_sweep) = First_array(:,1:Sample_per_sweep) - mean(First_array(:,1:Sample_per_sweep));
+First_array(:,1:Sample_per_sweep) = First_array(:,1:Sample_per_sweep) - mean(First_array(:,1:Sample_per_sweep),"all");
 figure(2)
-fftfirst = 10*log10(abs(fft(First_array,4*Sample_per_sweep,2)));
+fftfirst = 10*log10(abs(fft(First_array,5*Sample_per_sweep,2)));
 fftfirst = fftfirst(:,1:Sample_per_sweep*2);
 %Norm1
-maxall = max(fftfirst, [], 'all');
-fftfirst = fftfirst - maxall;
+%maxall = max(fftfirst, [], 'all');
+%fftfirst = fftfirst - maxall;
 %Norm2
-%maxrows = max(fftfirst,[], 2);
-%fftfirst = fftfirst - maxrows;
+maxrows = max(fftfirst,[], 2);
+fftfirst = fftfirst - maxrows;
 imagesc(velocities, timearray, fftfirst,[-20 0])
 xlim([0 30])
 
